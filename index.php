d26202d8981f3cab05ca9855f8aa0741cc4af283
simarpreet717 <simarpreet717@gmail.com>
2018-09-08 13:09:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Armata" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="https://cdn.rawgit.com/sirxemic/jquery.ripples/ee02d531/jquery.ripples-min.js"></script>
+	<script src="js/typed.js" type="text/javascript"></script>
+	<script src="js/animatescroll.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Tide Theme</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container" id="home">
+			<div class="ripples">
+			</div>
+			<div class="header">
+					<div class="header-left">
+						<span style="cursor: pointer" onclick="$('#home').animatescroll(400);"><a class="title-heading">TI</a>DE</span>
+					</div>
+					<div class="header-right">
+						<ul>
+							<li><a onclick="$('#home').animatescroll(400);">HOME</a></li>
+							<li><a onclick="$('#about').animatescroll(400);">ABOUT</a></li>
+							<li><a onclick="$('#service').animatescroll(400);">SERVICES</a></li>
+							<li><a onclick="$('#testimonials').animatescroll(400);">TESTIMONIALS</a></li>
+							<li><a onclick="$('#contact').animatescroll(400);">CONTACT</a></li>
+						</ul>
+					</div>
+					<div style="clear: both;">
+					</div>
+			</div>
+			<div class="tagline">
+				<h1>HELLO ! THIS IS <span class="title-heading" style="margin:0;">SIMARPREET SINGH</span></h1>
+				<h3 style="margin-top:1%;">I AM A PROFESSIONAL <span class="title-heading" id="element" style="font-weight: bold;"></span></h3>
+			</div>
+			<div class="circle">
+				<a onclick="$('#about').animatescroll(400);"><i class="fa fa-chevron-down" aria-hidden="true" style="color: white;"></i></a>
+			</div>
+		</div>
+		<div id="about" style="clear:both; padding:0 0 60px 0">
+		</div>
+		<div class="body-container">
+			<div class="about-heading">
+				<h1>ABOUT</h1>
+			</div>
+			<div class="body-left">
+				<div class="body-image">
+					<img src="IMG_20141126_150613.jpg">
+					<button class="resume-btn"><a href="resume.doc" download>DOWNLOAD RESUME</a></button>
+				</div>
+			</div>
+			<div class="body-right">
+				<div class="about-content">
+					<h2 class="title-heading">I AM SIMARPREET SINGH</h2>
+					<p>I have done my Post Graduation in MCA from Sri Sai College of Management, Amritsar with 88% aggregate marks. I have also made a project named as MakeMyFolio which is live under www.makemyfolio.com domain. It is a Multisite Network Web Application Development Platform to create Dynamic Websites. My Strength are Programming, Designing and Development Skills.</p>
+					<p>My Hobbies are playing cricket, Watching Movies, Travelling and interacting with latest technology. My career objective in life is to work in a dynamic professional environment with a growing organization and utilize my creativity and innovative thinking for benefit of the organization and myself.</p>
+				</div>
+				<div class="social-btn">
+					<img src="facebook.png" style="width:90%; height: 90%;" onclick="location.href='http://facebook.com';">
+				</div>
+				<div class="social-btn">
+					<img src="googleplus.png" style="width:90%; height: 90%;" onclick="location.href='http://gmail.com';">
+				</div>
+				<div class="social-btn">	
+					<img src="linkedin.png" style="width:90%; height: 90%;" onclick="location.href='http://linkedin.com';">
+				</div>
+				<div class="social-btn">	
+					<img src="tumblr.png" style="width:90%; height: 90%;" onclick="location.href='http://tumblr.com';">
+				</div>
+			</div>
+			<div id="service" style="clear:both;">
+			</div>
+		</div>
+		<div class="service-section">
+			<div class="about-heading">
+				<h1>SERVICES</h1>
+			</div>
+			<div class="service-section1">
+				<i class="fa fa-pencil" aria-hidden="true" style="color:#44B272; font-size: 60px;"></i>
+				<h2>UI DESIGN</h2>
+				<p>Today's World UI Design is considered to be a very important aspect for a good Web Designer. Effective UI Designer must have good knowledge of HTML5 ,CSS3, Jquery, Javascript as these all are used while UI Designing.</p>
+			</div>
+			<div class="service-section1">
+				<i class="fa fa-code" aria-hidden="true" style="color:#44B272; font-size: 60px;"></i>
+				<h2>PROGRAMMING</h2>
+				<p>Programming consists of logics made for problem solving using a particular Programming Language. Programming is very important to make website Dynamic. There are many languages for this purpose such as C, C++, PhP, Java, ASP, Python, Ruby, Perl etc.</p>
+			</div>
+			<div class="service-section1">
+				<i class="fa fa-desktop" aria-hidden="true" style="color:#44B272; font-size: 60px;"></i>
+				<h2>WEB DEVELOPMENT</h2>
+				<p>Web Development consists of Designing, Programming, Testing, Implementing and all other aspects. Working on live projects Web Development plays a major role as a web Developer has to go through each and every aspect of the project made.</p>
+			</div>
+			<div id="testimonials" style="clear: both;">
+			</div>
+		</div>
+		<div class="slideshow">
+			<div class="about-heading">
+				<h1>TESTIMONIALS</h1>
+			</div>
+			<div class="slideshow-container fade">
+				<div class="slideshow-img">
+					<img src="IMG_20141126_150613.jpg">
+				</div>
+				<div class="slideshow-text">
+						<p class="txt">Email Designer and Developer providing Fully Responsive HTML Emails and Mailchimp Templates. Experienced in Web Designing a, Web Development, Graphics Designing, UI Development, UX Development, SEO, Networking, Hardware, Software Development, testing (both black box and white box).</p>
+				</div>
+			</div>
+			<div class="slideshow-container fade">
+				<div class="slideshow-img">
+					<img src="IMG_20150313_14463999.jpg">
+				</div>
+				<div class="slideshow-text">
+						<p class="txt">I am here to help you create or update your WordPress website. I have more than 20 years of diverse Information Technology experience including website development. As you can see on the panel to the right, I am a top rated Web Developer here with 98-100% job success (it varies with Upwork updates).</p>
+				</div>			
+			</div>
+			<div class="slideshow-container fade">
+				<div class="slideshow-img">
+					<img src="IMG_20150222_13172334.jpg">
+				</div>
+				<div class="slideshow-text">
+						<p class="txt">Do you need fantastic-looking slides that help you achieve your business goals? There is no need to waste time wrestling with PowerPoint and other Adobe graphic suite if it’s not your bag, or you have bigger fish to fry. Avoid the curse of ugly, untidy, boring slides that don’t look or feel like your amazing business!</p>
+				</div>
+			</div>
+		</div>		
+			<div id="contact" style="clear:both; padding:0 0 60px 0">
+			</div>
+		<div class="contact-section">
+			<div class="about-heading">
+				<h1>CONTACT</h1>
+			</div>
+			<div class="contact-box">
+				<input type="text" name="text" class="contact-name" placeholder="Name">
+				<input type="E-Mail" name="E-Mail" class="contact-name" placeholder="E-Mail">
+				<textarea cols="25" placeholder="Message" class="contact-name" rows="4" st></textarea> 
+				<div class="about-heading">
+					<button class="contact-btn" onclick="alertmessage()">Send</button>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<p>© SIMARPREET SINGH-2017 | All Right Reserved</p>
+		</div>
+	</div>
+</body>
+<script>
+	$(document).ready(function(){
+	 	$('.ripples').ripples({
+    		resolution: 850,
+    		dropRadius: 20,
+    		perturbance: 0.2,
+    		interactive: true,
+    	});
+	});
+</script>
+<script>
+	var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+}
+</script>
+<script>
+	var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow-container");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+<script>
+	document.addEventListener("DOMContentLoaded", function(){
+		Typed.new("#element", {
+			strings: ["WEB DEVELOPER.", "WEB DESIGNER.", "PROGRAMMER."],
+			typeSpeed: 0
+		});
+	});
+</script>
+<script>
+	$(document).ready(function(){
+  		$(window).scroll(function(){
+   			if ($(window).scrollTop() > 100){
+    			$('.header').addClass("header-shuffle");
+			}
+			else {
+   	 			$('.header').removeClass("header-shuffle");
+			}
+ 		});
+	});
+</script>
+<script>
+	function alertmessage()
+	{
+		alert("Thank you for sending message");
+	}
+</script>
+</html>
